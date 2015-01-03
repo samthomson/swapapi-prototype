09753531bdc816453e7ab393fb27504c47941a6f
unknown <samt@samt.st>
2015-01-03 22:36:42 +0100

    look up targets from url

--- handler.php
+++ handler.php
@@ -1,4 +1,12 @@
 <?php
 
-	echo "routed";
+	$saSwaps = array("google" => "http://www.google.com", "samt.st" => "http://samt.st");
+
+	$sSwapRequest = trim($_SERVER["REQUEST_URI"], "/" );;
+
+	//print_r($saSwaps);
+
+	if(isset($saSwaps[$sSwapRequest])){
+		echo "retrieve ".$saSwaps[$sSwapRequest];
+	}
 ?>
